8f0d7cf0ffaf50e4b5f9c92791a55124bb2e21f1
SalmenDev <Dev@2019**>
2020-12-18 21:41:47 +0100

    get by id

--- backend2/server.js
+++ backend2/server.js
@@ -27,6 +27,22 @@ app.use(cors());
 const fieldRoute=require('./routes/fields.route');
 app.use('/api/fields',fieldRoute);
 
+
+
+app.post('/api/posts', function (req, res, next) {
+    var post = new Post({
+        name: req.body.username,
+        phone: req.body.phone,
+        location:req.body.location,
+        category:req.body.category,
+        img:req.body.img
+    })
+    post.save(function (err, post) {
+      if (err) { return next(err) }
+      res.json(201, post)
+    })
+  })
+
 const port=process.env.PORT||3003;
 const server=app.listen(port,()=>{
     console.log("connected to port",port);
